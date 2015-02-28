36a5f0791fb36f583b030593512565535f42630d
Zarkonnen <david.stark@zarkonnen.com>
2015-02-28 13:50:23 +0100

    viewport meta

--- sitegen.py
+++ sitegen.py
@@ -2,7 +2,7 @@ print """<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8"> 
-        <meta name="viewport" content = "width = device-width, initial-scale = 1.0, user-scalable = no" />
+        <meta name="viewport" content = "width = device-width, initial-scale = 0.5, user-scalable = no" />
         <title>Swiss Games Showcase</title>
         <link rel="stylesheet" href="style.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
