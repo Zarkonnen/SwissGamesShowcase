ac024d82e903a6d08e25bb8d73545769ed72ab31
Zarkonnen <david.stark@zarkonnen.com>
2017-09-17 16:53:08 +0200

    More games, better image scaling through using the browser.

--- Resize.java
+++ Resize.java
@@ -9,23 +9,23 @@ public class Resize {
         int h = Integer.parseInt(args[2]);
         double targetAspect = 1.0 * w / h;
         double sourceAspect = 1.0 * b.getWidth() / b.getHeight();
-        int x = 0;
-        int y = 0;
-        int tw = w;
-        int th = h;
+        int sw = b.getWidth();
+        int sh = b.getHeight();
+        int tx = 0;
+        int ty = 0;
+        int tw = b.getWidth();
+        int th = b.getHeight();
         if (sourceAspect > targetAspect) {
             // Want to cut off the sides.
-            tw = (int) Math.ceil(1.0 * b.getWidth() * h / b.getHeight());
-            x = w / 2 - tw / 2;
+            tw = w * th / h;
+            tx = tw / 2 - sw / 2;
         } else if (sourceAspect < targetAspect) {
             // Want to cut off top and bottom.
-            th = (int) Math.ceil(1.0 * b.getHeight() * w / b.getWidth());
-            y = h / 2 - th / 2;
+            th = h * tw / w;
+            ty = th / 2 - sh / 2;
         }
-        BufferedImage b2 = new BufferedImage(w * 10, h * 10, args[4].equals("png") ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_3BYTE_BGR);
-        b2.getGraphics().drawImage(b, x * 10, y * 10, tw * 10, th * 10, null);
-        BufferedImage b3 = new BufferedImage(w, h, args[4].equals("png") ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_3BYTE_BGR);
-        b23.getGraphics().drawImage(b2, 0, 0, w, h, null);
-        ImageIO.write(b3, args[4], new File(args[3]));
+        BufferedImage b2 = new BufferedImage(tw, th, args[4].equals("png") ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_3BYTE_BGR);
+        b2.getGraphics().drawImage(b, tx, ty, null);
+        ImageIO.write(b2, args[4], new File(args[3]));
     }
 }
